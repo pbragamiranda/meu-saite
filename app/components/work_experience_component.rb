5b91542061c416f8cbd11f47ab6c840d1606b989
pedro miranda <pbragamiranda@pm.me>
2023-03-21 11:34:24 +0100

    work

--- app/components/work_experience_component.rb
+++ app/components/work_experience_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class WorkExperienceComponent < ViewComponent::Base
-  def initialize(company:, position:, duration:)
+  def initialize(company:, position:, duration:, url:)
+    @url = url
     @company = company
     @position = position
     @duration = duration
